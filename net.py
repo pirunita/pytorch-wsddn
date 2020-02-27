9b7015c3876e0e25313bcd645716d00374fe8c89
pirunita <ksparchive39@gmail.com>
2020-02-27 10:10:03 +0900

    Edit network

--- net.py
+++ net.py
@@ -2,11 +2,15 @@ import math
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+BATCH_SIZE = 1
 
 class WSDDN(nn.Module):
     def __init__(self):
         super(WSDDN, self).__init__()
         
+        # Alexnet
         # pool5 for extracting features
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)),
@@ -34,7 +38,19 @@ class WSDDN(nn.Module):
     def forward(self, x, ssw_output):
         x = self.features(x)
         x = self.spp_layer(x, ssw_output)
-    
+        
+        x = F.relu(self.fc6(x))
+        x = F.relu(self.fc7(x))
+        x_clf = F.relu(self.fc8c(x))
+        x_dct = F.relu(self.fc8d(x))
+        sigma_clf = F.softmax(x_clf, dim=2)
+        sigma_dct = F.softmax(x_dct, dim=1)
+        
+        x = sigma_clf * sigma_dct
+        x = torch.sum(x, dim=1)
+        
+        return x, sigma_clf, sigma_dct
+        
     def spp_layer(self, x, ssw):
         #x.shape = [BATCH_SIZE, 512, 14, 14] ssw_get.shape = [BATCH_SIZE, R, 4] y.shape = [BATCH_SIZE, R, 4096]
         """
@@ -44,9 +60,26 @@ class WSDDN(nn.Module):
         """
         for i in range(BATCH_SIZE):
             for j in range(ssw.size(1)):
-        
-        
+                feature_map_piece = torch.unsqueeze(x[i, :, math.floor(ssw[i, j, 0]) : math.floor(ssw[i, j, 0] + ssw[i, j, 2]),
+                                               math.floor(ssw[i, j, 1]) : math.floor(ssw[i, j, 1] + ssw[i, j, 3])], 0)
+                feature_map_piece = spatial_pyramid_pool(previous_conv=feature_map_piece,
+                                                         num_sample=1,
+                                                         previous_conv_size = [feature_map_piece.size(2), feature_map_piece.size(3)],
+                                                         out_pool_size=[2, 2])
 
+                if j == 0:
+                    y_piece = feature_map_piece
+                else:
+                    y_piece = torch.cat((y_piece, feature_map_piece))
+            
+            if i == 0:
+                y = torch.unsqueeze(y_piece, 0)
+            else:
+                y = torch.cat((y, torch.unsqueeze(y_piece, 0)))
+        
+        return y
+        
+        
 def spatial_pyramid_pool(previous_conv, num_sample, previous_conv_size, out_pool_size):
     """
     previous_conv: a tensor vector of previous convolution layer
@@ -60,5 +93,16 @@ def spatial_pyramid_pool(previous_conv, num_sample, previous_conv_size, out_pool
     for i in range(len(out_pool_size)):
         h_wid = math.ceil(previous_conv_size[0] / out_pool_size[i])
         w_wid = math.ceil(previous_conv_size[1] / out_pool_size[i])
-        h_pad = min(math.floor((h_wid*out_pool_size[i])))
+        h_pad = min(math.floor((h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2), math.floor(h_wid/2))
+        w_pad = min(math.floor((w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2), math.floor(w_wid/2))
         
+        maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
+        
+        x = maxpool(previous_conv)
+        
+        if(i == 0):
+            spp = x.view(num_sample, -1)
+        else:
+            spp = torch.cat((spp, x.view(num_sample, -1)), 1)
+        
+        return spp
