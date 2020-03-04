8dc39cc10f8170545f69416cfd0a26aa35298542
pirunita <ksparchive39@gmail.com>
2020-03-04 09:51:22 +0900

    fixed train.py

--- train.py
+++ train.py
@@ -20,12 +20,14 @@ def get_args():
     parser.add_argument('--lr', type=float, default=0.0001, \
         help='Base learning rate for Adam')
     parser.add_argument('--epoch', type=int, default=40)
+    parser.add_argument('--datamode', default='train')
     
     # Directory
     parser.add_argument('--dataroot', default='data')
-    parser.add_argument('--datamode', default='train')
-    parser.add_argument('--data_list', default='annotations.txt')
-    parser.add_argument('--ssw_list', default='ssw.txt')
+    parser.add_argument('--jpeg_path', default='JPEGImages')
+    parser.add_argument('--text_path', default='annotations.txt')
+    parser.add_argument('--json_path', default='voc2007.json')
+    parser.add_argument('--ssw_path', default='ssw.txt')
     
     parser.add_argument('--pretrained_dir', type=str, default='pretrained', help='Load pretrained model')
     parser.add_argument('--checkpoint_dir', type=str, default='checkpoints', help='save checkpoint info')
@@ -51,43 +53,46 @@ def train(args, model):
     if not os.path.exists(args.checkpoint_dir):
         os.makedirs(args.checkpoint_dir)
         
-    with tqdm.tqdm(total=args.epoch) as progress:
-        if args.datamode == 'train':
-            train_data = WSDDNDataset(args)
-            train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=False, num_workers=1)
-            model.train()
-            for step in range(args.epoch):
-                running_loss = 0.0
-                for i, (images, ssw_block, labels) in enumerate(train_loader):
-                    images = Variable(images).cuda()
-                    ssw_block = Variable(ssw_block).cuda()
-                    labels = Variable(labels).cuda()
-                    if step < 10:
-                        optimizer1.zero_grad()
-                    else:
-                        optimizer2.zero_grad()
-                    
-                    output, output_clf, output_dct = model(images, ssw_block)
-                    output = torch.sigmoid(output)
-                    loss = criterion(output, labels)
-                    loss.backward()
-                    if step < 10:
-                        optimizer1.step()
-                    else:
-                        optimizer2.step()
-                    
-                    running_loss += loss.item()
-                    progress.set_description("Step: {step} Iteration: {iter} Loss: {loss}".format( \
-                        step=step, iter=i, loss=running_loss/500
-                    ))
-                    progress.update()
+    
+    if args.datamode == 'train':
+        train_data = WSDDNDataset(args)
+        train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=False, num_workers=1)
+        model.train()
+        for step in tqdm.tqdm(range(args.epoch)):
+            running_loss = 0.0
+            for i, (images, ssw_block, labels) in tqdm.tqdm(enumerate(train_loader)):
+                
+                images = Variable(images).cuda()
+                ssw_block = Variable(ssw_block).cuda()
+                labels = Variable(labels).cuda()
+                if step < 10:
+                    optimizer1.zero_grad()
+                else:
+                    optimizer2.zero_grad()
+                
+                output, output_clf, output_dct = model(images, ssw_block)
+                output = torch.sigmoid(output)
+                loss = criterion(output, labels)
+                loss.backward()
+                if step < 10:
+                    optimizer1.step()
+                else:
+                    optimizer2.step()
+                
+                running_loss += loss.item()
+                
+                """
+                print("Step: {step} Iteration: {iter} Loss: {loss}".format( \
+                    step=step, iter=i, loss=running_loss/500
+                ))
+                """
 
                 board.add_scalar('Train/loss', loss.item(), step)
-                torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, 'wsddn.pkl'))
-            
-            print('Finished Training')
-            board.close()
             torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, 'wsddn.pkl'))
+        
+        print('Finished Training')
+        board.close()
+        torch.save(model.state_dict(), os.path.join(args.checkpoint_dir, 'wsddn.pkl'))
                     
                     
     """
