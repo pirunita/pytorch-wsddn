e3138332a233ac56fd0a28adfcef793cd899a4cb
pirunita <ksparchive39@gmail.com>
2020-03-15 02:03:49 +0900

    Fixed bug

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def get_args():
     parser.add_argument('--dataroot', default='data')
     parser.add_argument('--jpeg_path', default='JPEGImages')
     parser.add_argument('--text_path', default='annotations.txt')
-    parser.add_argument('--json_path', default='voc2007.json')
+    parser.add_argument('--image_label_path', default='voc_2007_trainval_image_label.json')
     #parser.add_argument('--ssw_path', default='ssw.txt')
     parser.add_argument('--ssw_path', default='voc_2007_trainval.mat')
     
